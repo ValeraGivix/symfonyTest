f9f29acc9d25a53b700872496205300a13bca015
ValeraGivix <valera.ianac@gmail.com>
2018-08-27 17:16:08 +0300

    Working with private attributes.

--- test.php
+++ test.php
@@ -3,21 +3,71 @@
 
 class Worker
 {
-    public $name;
-    public $age;
-    public $salary;
+    private $name;
+    private $age;
+    private $salary;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age): void
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @param mixed $salary
+     */
+    public function setSalary($salary): void
+    {
+        $this->salary = $salary;
+    }
+
 }
 
 $one = new Worker();
 
-$one->name = 'Ivan';
-$one->age = 25;
-$one->salary = 1000;
+$one->setName('Ivan');
+$one->setAge(25);
+$one->setSalary(1000);
 
 $two = new Worker();
-$two->name = 'Vasia';
-$two->age = 26;
-$two->salary = 2000;
 
-print $one->salary + $two->salary;
-print $one->age + $two->age;
+$two->setName('Vasia');
+$two->setAge(26);
+$two->setSalary(2000);
+
+print $one->getSalary() + $two->getSalary();
+print $one->getAge() + $two->getAge();
